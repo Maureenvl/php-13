e4ab986686c0d2ce6b626d9e78a8574128ae0a84
Maureen <220000@student.glu.nl>
2023-11-08 12:25:34 +0100

    kankerkaas

---
+++ php 13/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+   <a href ="detailpagina.php
+"><div class="project"><h1> Project 1</h1></div></a>
+</body>
+</html>
